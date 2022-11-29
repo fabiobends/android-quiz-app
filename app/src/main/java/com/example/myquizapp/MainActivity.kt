a78ab84aca8b19cdc5dfcc4ac093167e4240f8cb
Fábio Mendes <fabiobends@gmail.com>
2022-11-29 11:59:09 -0300

    feat: add "Question" action and navigation from a button
    - Add ids from button and text input
    - Create activity (screen) for questions
    - Use button to trigger a navigation to `QuizQuestionsActivity`
    - Create a model using data class
    - Create a constant wtth questions (object)
    - Use Logcat to examine data at `QuizQuestionsActivity`

--- app/src/main/java/com/example/myquizapp/MainActivity.kt
+++ app/src/main/java/com/example/myquizapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.myquizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val buttonStart: Button = findViewById(R.id.button_start)
+    val textName: EditText = findViewById(R.id.text_name)
+
+    buttonStart.setOnClickListener {
+      if (textName.text.isEmpty()) {
+        Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show()
+      } else {
+        val intent = Intent(this, QuizQuestionsActivity::class.java)
+        startActivity(intent)
+      }
+    }
   }
 }
